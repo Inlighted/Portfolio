fa495cd01a77d69395b4171fa9a56fef99106726
Priya <deshpande.priya07@gmail.com>
2026-08-04 15:32:05 -0700

    remove leftover blog import

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Education from "@/components/Education";
 import Certifications from "@/components/Certifications";
 import LeetCode from "@/components/LeetCode";
 import GitHubSection from "@/components/GitHubSection";
-import Blog from "@/components/Blog";
+
 import Testimonials from "@/components/Testimonials";
 import Services from "@/components/Services";
 import Contact from "@/components/Contact";
